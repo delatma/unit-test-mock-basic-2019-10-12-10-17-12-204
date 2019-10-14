d722fdcd216a66db51def46796bb1b4ac606e5c7
MARK ANTHONY DE LA TORRE (GEDI-ISD-OOCLL/MNL) <DELATMA@OOCL.COM>
2019-10-14 11:37:16 +0800

    finish all test cases using mockito and stub

--- src/test/java/posmachine/PosMachineTest.java
+++ src/test/java/posmachine/PosMachineTest.java
@@ -1,7 +1,12 @@
 package posmachine;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class PosMachineTest {
 
@@ -9,22 +14,47 @@ public class PosMachineTest {
     @Test
     public void should_get_receipt_using_real_price_calculator() {
         //given
-        //when
+        PriceCalculator calc = new PriceCalculator();
+        PosMachine myPosMachine = new PosMachine(calc);
+
         //then
+        Assertions.assertThrows(UnsupportedOperationException.class,() ->{
+            myPosMachine.getReceipt("Test");
+        });
     }
 
     @Test
-    public void should_get_receipt_using_sub_price_calculator() {
+    public void should_get_receipt_using_stub_price_calculator() {
         //given
+        PriceCalculator calc = new StubPriceCalculator();
+        PosMachine myPosMachine = new PosMachine(calc);
+
         //when
+        String receipt = myPosMachine.getReceipt("Peanuts");
+
         //then
+        Assertions.assertEquals("Name: Peanuts, Price: 45.0", receipt);
     }
 
     @Test
     public void should_get_receipt_using_real_price_calculator_with_mockito() {
         //given
+        PriceCalculator mockedCalculator = mock(PriceCalculator.class);
+        PosMachine myPosMachine = new PosMachine(mockedCalculator);
+        when(mockedCalculator.calculate(eq("Peanuts"))).thenReturn(14.0);
+
         //when
+        String receipt = myPosMachine.getReceipt("Peanuts");
+
         //then
+        Assertions.assertEquals("Name: Peanuts, Price: 14.0", receipt);
     }
 
+
+    private class StubPriceCalculator extends PriceCalculator {
+        @Override
+        public double calculate(String productName) {
+            return 45.0;
+        }
+    }
 }
